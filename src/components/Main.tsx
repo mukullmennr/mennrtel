1de400814c34936c4f5a0e7d2d819864d5149fdf
Anirudh Saini <anirudh.saini852@gmail.com>
2024-01-15 12:40:42 +0530

    home page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,54 +1,57 @@
 import React from "react";
 const Main = () => {
-	return (
-		<>
-			<div className="container">
-				<div className="main">
-					<div className="main_heading">
-						<h1>
-							We're passionate about helping{" "}
-							<span>businesses succeed</span> online
-						</h1>
-						<p>
-							Lucky for you, <span>digital marketing</span> is our
-							forte.
-						</p>
-					</div>
-					<div>
-						<img src="/main/image.svg" alt="image" />
-					</div>
-				</div>
-			</div>
-			<div className="main-image">
-				<div className="container">
-					<div className="main-image__item">
-						<img
-							src="/main/Google_certificate.svg"
-							alt="Google_certificate"
-						/>
-					</div>
+    return (
+        <>
+            <div className="container">
+                <div className="main">
+                    <div className="main_heading">
+                        <h1 className="main_heading1">
+                            We're passionate about helping{" "}
+                            <span>business succeed</span> online
+                        </h1>
+                        <p className="main_heading2">
+                            Lucky for you, <span>digital marketing</span> is our
+                            forte.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/main/image.svg" alt="image" />
+                    </div>
+                </div>
+            </div>
+            <div className="main-image">
+                <div className="container">
+                    <div className="silder">
+                        <div className="slides">
+                            <div className="slide_first">
+                                <img
+                                    src="/main/Google_certificate.svg"
+                                    alt="Google_certificate"
+                                />
+                            </div>
 
-					<div className="main-image__item">
-						<img
-							src="/main/hubspot_certificate.svg"
-							alt="hunspot_certificate"
-						/>
-					</div>
+                            <div className="slide">
+                                <img
+                                    src="/main/hubspot_certificate.svg"
+                                    alt="hunspot_certificate"
+                                />
+                            </div>
 
-					<div className="main-image__item">
-						<img
-							src="/main/Yext_certificate.svg"
-							alt="Yext_certificate"
-						/>
-					</div>
-
-					<div className="main-image__text">
-						<p>Yes, we're certified</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+                            <div className="slide">
+                                <img
+                                    src="/main/Yext_certificate.svg"
+                                    alt="Yext_certificate"
+                                />
+                            </div>
+                        </div>
+                        <div className="main-image__text">
+                            <p>Yes, we're certified</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Main;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,37 +1,64 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import icon from "@/../public/icon.svg";
 import Link from "next/link";
 import { inter } from "@/fonts/fonts";
 
 export default function Nav() {
-	return (
-		<div className={`${inter.className} border`}>
-			<div className="container">
-				<div className="nav">
-					<div>
-						<Image src={icon} alt="mennrtel" />
-					</div>
-					<div className="nav_menu">
-						<ul>
-							<li>
-								<Link href="/">Who We Are</Link>
-							</li>
-							<li>
-								<Link href="/">What We Do</Link>
-							</li>
-							<li>
-								<Link href="/">Who We Help</Link>
-							</li>
-							<li>
-								<Link href="/">Solutions</Link>
-							</li>
-						</ul>
-						<button>Get Started</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    // const nav = document.querySelector(".border") ;
+    // let lastScrollY = window.scrollY;
+    // window.addEventListener("scroll", () => {
+    //     if (lastScrollY < window.scrollY) {
+    //         nav.classList.add("nav--hidden");
+    //     } else {
+    //         nav.classList.remove("nav--hidden");
+    //     }
+
+    //     lastScrollY = window.scrollY;
+    // });
+    return (
+        <div className={`${inter.className} border`}>
+            <div className="container">
+                <div className="nav">
+                    <div className="nav_icon">
+                        <div>
+                            <input
+                                type="checkbox"
+                                id="Toggle"
+                                // onClick={handleCheckboxClick}
+                            />
+                            <label htmlFor="Toggle">
+                                <div className="Menu-container">
+                                    <div className="line" id="active"></div>
+                                </div>
+                            </label>
+                        </div>
+                        <div className="nav_img">
+                            <Link href="/">
+                                <img src="/icon.svg" alt="mennrtel" />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="nav_menu">
+                        <ul id="menu">
+                            <li>
+                                <Link href="/">Who We Are</Link>
+                            </li>
+                            <li>
+                                <Link href="/">What We Do</Link>
+                            </li>
+                            <li>
+                                <Link href="/">Who We Help</Link>
+                            </li>
+                            <li>
+                                <Link href="/">Solutions</Link>
+                            </li>
+                        </ul>
+                        <div>
+                            <button>Get free email quote</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
