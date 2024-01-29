bb743f1f06399c3dfe1ee695112f0f858c873d90
rohan <rohanverma031@gmail.com>
2024-01-29 12:09:57 +0530

    solution list design fixed

--- src/components/Sol_list.tsx
+++ src/components/Sol_list.tsx
@@ -4,69 +4,88 @@ import React from "react";
 import * as Accordion from "@radix-ui/react-accordion";
 
 interface Ques {
-    question: string;
-    answer: string;
+	question: string;
+	answer: string;
 }
 
 interface FAQProps {
-    children: React.ReactNode;
-    ques: Ques[];
+	children: React.ReactNode;
+	ques: Ques[];
 }
 
 interface AccordionElement {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 function AccordionTrigger({ children, ...props }: AccordionElement) {
-    return (
-        <Accordion.Header className={styles.accordion_header}>
-            <Accordion.Trigger className={styles.accordion_trigger} {...props}>
-                <p>{children}</p>
-                <div>
-                    <span></span>
-                    <span></span>
-                </div>
-            </Accordion.Trigger>
-        </Accordion.Header>
-    );
+	return (
+		<Accordion.Header className={styles.accordion_header}>
+			<Accordion.Trigger className={styles.accordion_trigger} {...props}>
+				<p>{children}</p>
+				<div>
+					<span></span>
+					<span></span>
+				</div>
+			</Accordion.Trigger>
+		</Accordion.Header>
+	);
 }
 
 function AccordionContent({ children, ...props }: AccordionElement) {
-    return (
-        <Accordion.Content {...props} className={styles.accordion_content}>
-            {children}
-        </Accordion.Content>
-    );
+	return (
+		<Accordion.Content {...props} className={styles.accordion_content}>
+			{children}
+		</Accordion.Content>
+	);
 }
 
 export default function FAQ({ children, ques }: FAQProps) {
-    const accordionItems = ques.map((item) => {
-        return (
-            <Accordion.Item
-                key={item.question}
-                className={styles.accordion_item}
-                value={item.question}
-            >
-                <AccordionTrigger>{item.question}</AccordionTrigger>
+	let len = ques.length;
 
-                <AccordionContent>{item.answer}</AccordionContent>
-            </Accordion.Item>
-        );
-    });
+	const accordionItems = ques.map((item, index) => {
+		if (index >= len / 2) return;
+		return (
+			<Accordion.Item
+				className={styles.accordion_item}
+				value={item.question}
+				key={item.question}
+			>
+				<AccordionTrigger>{item.question}</AccordionTrigger>
 
-    return (
-        <div className={styles.faq}>
-            <h3 className={styles.faq_heading}>{children}</h3>
+				<AccordionContent>{item.answer}</AccordionContent>
+			</Accordion.Item>
+		);
+	});
 
-            <div className={styles.faq_accordion}>
-                <Accordion.Root
-                    className={styles.accordion_root}
-                    type="single"
-                    collapsible
-                >
-                    {accordionItems}
-                </Accordion.Root>
-            </div>
-        </div>
-    );
+	const accordionItems1 = ques.map((item, index) => {
+		if (index < len / 2) return;
+		return (
+			<Accordion.Item
+				className={styles.accordion_item}
+				value={item.question}
+				key={item.question}
+			>
+				<AccordionTrigger>{item.question}</AccordionTrigger>
+
+				<AccordionContent>{item.answer}</AccordionContent>
+			</Accordion.Item>
+		);
+	});
+
+	return (
+		<div className={styles.faq}>
+			<h3 className={styles.faq_heading}>{children}</h3>
+
+			<div className={styles.faq_accordion}>
+				<Accordion.Root
+					className={styles.accordion_root}
+					type="single"
+					collapsible
+				>
+					<div>{accordionItems}</div>
+					<div>{accordionItems1}</div>
+				</Accordion.Root>
+			</div>
+		</div>
+	);
 }
--- src/data/components/sol_list.js
+++ src/data/components/sol_list.js
@@ -1,45 +1,45 @@
 export const sol = {
-    heading: "Mennrtel Marketing Solutions",
-    data: [
-        {
-            question: "Strategy",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Advertising",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Website Design",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Social Media",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Content  Marketing",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Branding",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "SEO",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Email Marketing",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Analytics",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-        {
-            question: "Reputation Management",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
-        },
-    ],
+	heading: "Mennrtel Marketing Solutions",
+	data: [
+		{
+			question: "Strategy",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Advertising",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Website Design",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Social Media",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Content  Marketing",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Branding",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "SEO",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Email Marketing",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Analytics",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+		{
+			question: "Reputation Management",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed libero et rhoncus. Etiam varius tincidunt velit, quis dapibus enim dapibus eu. Aliquam erat volutpat. Vivamus dictum, erat in finibus egestas, quam tortor malesuada ipsum, quis porta risus eros non elit.",
+		},
+	],
 };
